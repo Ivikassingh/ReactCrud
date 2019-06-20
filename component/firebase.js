033dc07ec6e974f1748f1c92529929a268da5a7d
Vikas <vikas.r.kumar.9@gmail.com>
2019-06-20 17:46:37 +0530

    new changes

--- component/firebase.js
+++ component/firebase.js
@@ -4,15 +4,15 @@ import firestore from "firebase/firestore";
 const settings = { timestampsInSnapshots: true };
 
 const config = {
-  apiKey: "AIzaSyAVQRqcwm2zCHAoBa_f5KVhldg0MBg7y6o",
-  authDomain: "clifiml.firebaseapp.com",
-  databaseURL: "https://clifiml.firebaseio.com",
-  projectId: "clifiml",
-  storageBucket: "clifiml.appspot.com",
-  messagingSenderId: "405132876749",
-  appId: "1:405132876749:web:897deb6b4c38208d"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
-// firebase.initializeApp(config);
+ firebase.initializeApp(config);
 
 firebase.firestore().settings(settings);
 
